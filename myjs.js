eb4f789e58f42e78cf76301ddbdc1b0f0a7183e7
givanov95 <63155374+givanov95@users.noreply.github.com>
2020-05-22 17:46:30 +0300

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -1,15 +1,51 @@
-	var pages = document.getElementsByClassName('page');
-	function menuOpener() {				
-		let transformPercents = 30;
-		
+	
+	const menuOpener = document.querySelector(".js--menuOpener");
+	const pages = document.getElementsByClassName('page');
+	const menuContainer = document.querySelector('.menu-container');
+	const menuNames = document.querySelectorAll('.menu-container span');
 
-			for (var i = 0; i < pages.length; i++) {		
+			for (let i = 0; i < menuNames.length; i++) {
+				menuNames[i].addEventListener("click", function(event){
+				
+				let thisElement = document.getElementById(this.innerHTML);
+			
+			for (var z = 0; z < pages.length; z++) {
+					pages[z].classList.remove(['menu-checked'], ['shadow-lg']);
+					pages[z].style.transform="none";
+					pages[z].style.display="none";
+
+				}
+
+				thisElement.style.display="block";			
+				menuContainer.classList.remove("menu-opened");
+				thisElement.classList.add("animation-divs");
+			
+
+		});
+	}
+	
+
+	menuOpener.addEventListener("click", function(){
+
+
+
+			let transformPercents = 30;	
+				
+
+			menuContainer.classList.add("menu-opened");
+
+			for (let i = 0; i < pages.length; i++) {
+			if(pages[i].style.display="inline-block"){ 
+				
+			}
+				
 			pages[i].style.display="inline-block";						
 			pages[i].classList.add(['menu-checked'], ['shadow-lg']);
 			pages[i].style.transform=" translate3d(0px,"+ transformPercents+"%, -200px)";
-			transformPercents-=(i+1)*30;		
-			pages[i].addEventListener("click", function(){
+			transformPercents-=(i+1)*30+(20*i);	
+			
 
+			pages[i].addEventListener("click", function(){
 				for (var z = 0; z < pages.length; z++) {
 					pages[z].classList.remove(['menu-checked'], ['shadow-lg']);
 					pages[z].style.transform="none";
@@ -17,13 +53,15 @@
 
 				}
 
-				this.style.display="block";
-				this.classList.add("animation-divs")
+				this.style.display="block";			
+				menuContainer.classList.remove("menu-opened");
+				this.classList.add("animation-divs");
 			
 			});
 
+
 		}
-	}
+	});	
 
 	function hideModal(element) { 
 
